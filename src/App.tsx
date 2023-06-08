a9458af579cbc80f4154442df65b1a707b105ea8
TatianeMarinho <devtatianemarinho@gmail.com>
2023-06-08 10:32:34 -0400

    02

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+import Form from './components/Form';
 import Title from './components/Title';
 
 function App() {
   return (
     <main>
       <Title />
+      <section>
+        <Form />
+      </section>
     </main>
   );
 }
